9df41104eb069552c20ec9173e7398d754a94ec8
AndresTorrez <andres.torrez.ulloa@gmail.com>
2025-11-07 23:58:27 -0400

    Ayuda

---
+++ TecWeb.Infrastructure/Data/Configurations/UsuarioConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using TecWeb.Core.Entities;
+
+namespace TecWeb.Infrastructure.Data.Configurations
+{
+    public class UsuarioConfiguration : IEntityTypeConfiguration<Usuario>
+    {
+        public void Configure(EntityTypeBuilder<Usuario> builder)
+        {
+
+        }
+    }
+    
+}
